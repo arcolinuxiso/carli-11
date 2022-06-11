bb218a66e11ac4c1b94063039613b319cde3b943
arcolinuxz <arcolinuxinfo@gmail.com>
2022-06-11 08:54:20 +0200

    update

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -46,7 +46,7 @@ echo
 	isoLabel='carli-'$(date +%Y.%m.%d)'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 63-2"
+	archisoRequiredVersion="archiso 64-1"
 	buildFolder=$HOME"/carli-build"
 	outFolder=$HOME"/Carli-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
