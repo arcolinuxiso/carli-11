5afb53cf37b16e4abe9716b882a442a765179ec0
arcolinuxz <arcolinuxinfo@gmail.com>
2022-12-02 18:38:22 +0100

    extra script

---
+++ 9-get-all-arcolinux-carli-gits-v1.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author 	: Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website	: https://www.arcolinux.info
+# Website	: https://www.arcolinux.com
+# Website	: https://www.arcolinuxd.com
+# Website	: https://www.arcolinuxb.com
+# Website	: https://www.arcolinuxiso.com
+# Website	: https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black 
+#tput setaf 1 = red 
+#tput setaf 2 = green
+#tput setaf 3 = yellow 
+#tput setaf 4 = dark blue 
+#tput setaf 5 = purple
+#tput setaf 6 = cyan 
+#tput setaf 7 = gray 
+#tput setaf 8 = light blue
+##################################################################################################################
+
+echo "This updates the existing githubs"
+echo "Fill the array with the original folders first"
+
+# use ls -d */ > list to get the list of the created githubs and copy/paste in
+
+directories=(
+carli-1/
+carli-2/
+carli-3/
+carli-4/
+carli-5/
+carli-6/
+carli-7/
+carli-8/
+carli-9/
+carli-10/
+carli-11/
+carli-calamares-config/
+carli-calamares-qml-config/
+carli-grub-poly-light/
+carli-mirrorlist/
+carli-pkgbuild/
+carli_repo/
+carli-system-installation/
+)
+
+
+
+count=0
+
+for name in "${directories[@]}"; do
+	count=$[count+1]
+	tput setaf 1;echo "Github "$count;tput sgr0;
+	# if there is no folder then make one
+	git clone https://github.com/arcolinuxiso/$name
+	echo "#################################################"
+	echo "################  "$(basename `pwd`)" done"
+	echo "#################################################"
+done
