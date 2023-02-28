5c7e5fbd563f0cf08562d1000ebfcae47eb76a15
arcolinuxz <arcolinuxinfo@gmail.com>
2023-02-28 09:53:40 +0100

    archiso 70

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -46,7 +46,7 @@ echo
 	isoLabel='carli-'$(date +%Y.%m.%d)'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 69-1"
+	archisoRequiredVersion="archiso 70-1"
 	buildFolder=$HOME"/carli-build"
 	outFolder=$HOME"/Carli-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
